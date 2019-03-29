0ec5324bfbc47243c8a16cc54832693e6351a970
xiangxiaohai@b1024.cn <xiangxiaohai@b1024.cn>
2019-03-29 12:01:43 +0800

    modify	1

--- shell/conn_mysql.sh
+++ shell/conn_mysql.sh
@@ -3,21 +3,21 @@
 
 case $1 in
  p|processlist)
-        /app/mysqld/bin/mysqladmin --user=root --host=localhost --socket=/tmp/mysql.sock --port=3388 --password processlist
+        /app/mysqld/bin/mysqladmin --user=root --host=localhost --socket=/tmp/mysql.sock --port=xxx --password processlist
 ;;
  v|variables)
-        /app/mysqld/bin/mysqladmin --user=root --host=localhost --socket=/tmp/mysql.sock --port=3388 --password variables
+        /app/mysqld/bin/mysqladmin --user=root --host=localhost --socket=/tmp/mysql.sock --port=xxx --password variables
 ;;
  s|status)
-        /app/mysqld/bin/mysqladmin --user=root --host=localhost --socket=/tmp/mysql.sock --port=3388 --password status
+        /app/mysqld/bin/mysqladmin --user=root --host=localhost --socket=/tmp/mysql.sock --port=xxx --password status
 ;;
  t|Threads_connected)
-        ###/app/mysqld/bin/mysql --user=root --host=localhost --socket=/tmp/mysql.sock --port=3388 --password --execute="show status like '%Threads_connected%'\G;"#####|awk -F':' '/Value/ {print $NF}'
-        /app/mysqld/bin/mysql --user=root --host=localhost --socket=/tmp/mysql.sock --port=3388 --password --execute="show status like '%Threads_connected%';"
+        ###/app/mysqld/bin/mysql --user=root --host=localhost --socket=/tmp/mysql.sock --port=xxx --password --execute="show status like '%Threads_connected%'\G;"#####|awk -F':' '/Value/ {print $NF}'
+        /app/mysqld/bin/mysql --user=root --host=localhost --socket=/tmp/mysql.sock --port=xxx --password --execute="show status like '%Threads_connected%';"
 ;;
  th|Threads_running)
-        ###/app/mysqld/bin/mysql --user=root --host=localhost --socket=/tmp/mysql.sock --port=3388 --password --execute="show status like '%Threads_running%'\G;"######|awk -F ':' '/Value/ {print $NF}'
-        /app/mysqld/bin/mysql --user=root --host=localhost --socket=/tmp/mysql.sock --port=3388 --password --execute="show status like '%Threads_running%';"
+        ###/app/mysqld/bin/mysql --user=root --host=localhost --socket=/tmp/mysql.sock --port=xxx --password --execute="show status like '%Threads_running%'\G;"######|awk -F ':' '/Value/ {print $NF}'
+        /app/mysqld/bin/mysql --user=root --host=localhost --socket=/tmp/mysql.sock --port=xxx --password --execute="show status like '%Threads_running%';"
 ;;
  *|--help)
         echo "$Usage: $0 { p(processlist) | s(status) | v(variables) | t(Threads_connected) | th(Threads_running) } "
