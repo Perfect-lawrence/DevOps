94f424fd78d7b4cb593682dd678f01eff78a4725
xiangxiaohai@b1024.cn <xiangxiaohai@b1024.cn>
2019-09-18 10:34:31 +0800

    modified

--- shell/urlCheck.sh
+++ shell/urlCheck.sh
@@ -8,9 +8,9 @@ for((i=0;i<$web_length;i++))
 do
 pj=$(/usr/bin/curl -I --connect-timeout 15 -o /dev/null -s -w %{http_code} "${web[$i]}")
 
-if [ "${pj}" -eq 200 ]; then
-    echo "${pj} url ok"
-else
+if [ "${pj}" -eq 000 ]; then
+	exit 111
+elif [ "${pj}" -ne 200 -o "${pj}" -gt 200 ]; then
     echo "${web[$i]} url status code:${pj}" |mailx -s "dkfdkds" $rec
 fi
 done 
@@ -23,9 +23,9 @@ for((i=0;i<$web2_length;i++))
 do
 	sleep 1
 	infox=$(/usr/bin/curl -I --connect-timeout 15 -o /dev/null -s -w %{http_code} "${web2[$i]}")
-	if [ "${infox}" -ne 200 ]; then
-    #echo "${infox} url ok"
-#else
-    echo "${web2[$i]} url status code:${infox}"
-fi
+	if [ "${infox}" -eq 000 ]; then
+		exit 111
+	elif [ "${infox}" -ne 200 -o "${infox}" -gt 200 ] then
+   		 echo "${web2[$i]} url status code:${infox}"
+	fi
 done 
